daf3fc9647015adaac5a8c2fb96e8a1297a63664
pritesh009 <77330382+pritesh@users.noreply.github.com>
2021-01-12 11:40:33 +0000

    TestUpdate
    TestUpdate

---
+++ Spyder Practice codes v1.py
@@ -0,0 +1,241 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+
+Github clone
+"""
+# =============================================================================
+#
+# quote = "Talk is Cheap.Show me the code."
+# print("1.",quote[3])
+# print("1.",quote[-3])
+# print("3.",quote.replace("code","program"))
+#
+# def FunctionName(name):
+#     print ("Artist :" + "A R Rahman", name)
+#     print ("Genre :" + "Jazz", name)
+#     print ("Year :" + "1999",name)
+#
+# FunctionName("Dave")
+# def FunctionGenre():
+#     print ( "Genre : Jazz" )
+#
+# def FunctionArtist():
+#     print ( "Artist : David Bennet" )
+#
+# def FunctionYear():
+#     print("Year : 1999")
+# =============================================================================
+
+# FunctionGenre()
+# FunctionArtist()
+# FunctionYear()
+# =============================================================================
+#
+# def genre():
+#     print ( "genre : Jazz" )
+#
+# def artist():
+#     print ( "artist : David Bennet" )
+#
+# def year():
+#     print("year : 1999")
+#
+# genre()
+# artist()
+# year()
+#
+# artistName = "David Bennet"
+#
+# print("David" in artistName)
+# print("Dave" in artistName)
+#
+# if artistName == ("David Bennet"):
+#     print("True")
+# End
+#
+# if artistName == ("Dave Bennet"):
+#     print("False")
+#
+# =========================================================
+
+# =============================================================================
+#
+#
+# score = int(input("Enter your score :"))
+#
+# if score >= 50:
+#         print("You have passed your exam")
+#         print("Congratulations !!")
+#
+# if score < 50:
+#         print("Sorry, You have FAILED your exam")
+# =============================================================================
+
+# =============================================================================
+# score = int(input("Enter your score :"))
+#
+#
+# if score >100 or score < 0:
+#         print("Invalid score")
+#
+# elif score >= 50:
+#         print("You have passed your exam")
+#         print("Congratulations !!")
+#
+# else:
+#         print("Sorry, You have FAILED your exam")
+# =============================================================================
+
+# =============================================================================
+# num = 3
+# if num > 0:
+#     print(num, "is a positive number.")
+# print("This is always printed.")
+#
+# num = -1
+# if num > 0:
+#     print(num, "is a positive number.")
+# print("This is also always printed.")
+# =============================================================================
+
+# =============================================================================
+# 
+# num = 0
+# 
+# # Try these two variations as well:
+# # num = 0
+# # num = -4.5
+# 
+# if num > 0:
+#     print("Positive number")
+# elif num == 0:
+#     print("Zero")
+# else:
+#     print("Negative number")
+# 
+# =============================================================================
+
+# =============================================================================
+# text = "Python"
+# 
+# for character in text:
+#     print(character)
+# =============================================================================
+
+# =============================================================================
+# languages = ["English","hindi","Gujarati"]
+# 
+# 
+# for language in languages:
+#     print(language)
+# =============================================================================
+
+# =============================================================================
+# count =1
+# 
+# while count <10:
+#     print (count)
+#     count = count+1
+# =============================================================================
+    
+# =============================================================================
+# for count in range(1,6):
+#     print(count)
+# =============================================================================
+# =============================================================================
+# 
+# number = int(input("Enter your number: "))
+# 
+# for count in range (1,21):
+#     product = number * count
+#     print(number,'x',count,'=',product)
+# 
+# =============================================================================
+# =============================================================================
+# num = int(input("Enter your number: "))
+#  
+# 
+# if num > 0:
+#     print("Positive number")
+# elif num == 0:
+#     print("Zero")
+# else:
+#     print("Negative number")
+# =============================================================================
+
+# =============================================================================
+# 
+# count = 0
+# 
+# while count < 5:
+#     print("Inside loop")
+#     print("looping")
+#     print(count)
+#     count = count +1
+# 
+# =============================================================================
+
+# =============================================================================
+# num = int(input("Enter your number: "))
+# 
+# count =1
+# 
+# while count <=10:
+#     product = num * count
+#     print(num, "x",count,"=",product)
+#     count = count +1
+# =============================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
